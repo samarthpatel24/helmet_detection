37510ef71b9b78e986d85ef9d2f231f19019e35b
Matrix <113380979+samarthpatel24@users.noreply.github.com>
2023-10-18 23:24:53 +0530

    Update contact.py

--- contact.py
+++ contact.py
@@ -6,4 +6,4 @@ def app():
     st.markdown("# Contact:")
     st.markdown("Created by:")
     st.markdown('[Samarth Patel](https://github.com/samarthpatel24) , [Sameer Khatwani](https://github.com/samv28) , [Saharsh Mehrotra](https://github.com/saharshmehrotra) , [Rushil Shah](https://github.com/rushil23448)')
-    st.markdown('Contact via mail: [sammatrix2415@gmail.com]')
+
