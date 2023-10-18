68e1371304d75055d8f6339ca2032f5ad8225a6b
Matrix <113380979+samarthpatel24@users.noreply.github.com>
2023-10-18 21:12:17 +0530

    Update contact.py

--- contact.py
+++ contact.py
@@ -1,7 +1,9 @@
 import streamlit as st
 
+
+
 def app():
     st.markdown("# Contact:")
     st.markdown("Created by:")
-    st.markdown('[Samarth Patel](https://github.com/samarthpatel24) , [Sameer Khatwani](https://github.com/samarthpatel24) , [Saharsh Mehrotra](https://github.com/samarthpatel24) , [Rushil Shah](https://github.com/samarthpatel24)')
+    st.markdown('[Samarth Patel](https://github.com/samarthpatel24) , [Sameer Khatwani](https://github.com/samv28) , [Saharsh Mehrotra](https://github.com/saharshmehrotra) , [Rushil Shah](https://github.com/rushil23448)')
     st.markdown('Contact via mail: [sammatrix2415@gmail.com]')
